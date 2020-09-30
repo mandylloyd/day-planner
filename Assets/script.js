f2e785298e266db07e42c70f21ab2e6198e29496
honeybee <63674362+mandylloyd@users.noreply.github.com>
2020-09-29 22:27:38 -0400

    html structure made and date and time functioning created and displayed properly

--- Assets/script.js
+++ Assets/script.js
@@ -1,49 +1,27 @@
-    //TODO:
+//updates date and time on page load
+var hoursArr = ["9am", "10am", "11am", "12pm", "1pm", "2pm,", "3pm","4pm", "5pm"] ;
+
+var datetime = null,
+        date = null;
+        time = null;
+
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+
+$(document).ready(function(){
+    datetime = $('#currentDay')
+    update();
+    setInterval(update, 1000);
+});
     
-    //update date and time function
+//TODO
 
-    //function to change css styles to timeblocks if they are past, present, future compared to current time
+//function to add css style attributes to past, present, future time blocks by comparing current time to time block
 
-    //function that adds/saves new events to the local storage 
-  
-    //function to get old events from local storage and displays in proper blocks
+//function to save event text input and store it to local
 
-    //function to clear the event textarea on the screen and from local storage 
+//function to clear event text input and clear it from local
 
-    //function to update time in seconds.. !!needs to be displayed!!
-
-    $(document).ready(function() {
-
-        //variables
-        var timeNowDisplay; 
-        var timeAndDateNow; 
-        
-        //functions called when page loads
-        updateTime();
-
-        //updates date and time on page load
-        function updateTime () {
-            dateAndTime = $("#currentDay"); 
-            date = moment().format('MMMM Do YYYY');
-            time = moment().format('h:mm a');
-            dateAndTime.text("Date: " + date + " Time: " + time); 
-        }
-
-        //function to add css style attributes to past, present, future time blocks
-        // function blockStyles () {
-        // }
-
-        //function to save event text input and store to local
-
-        saveBtn.onclick = function saveEvent () {
-        var input = document.getElementById("textarea");
-        localStorage.setItem("event", input.val());
-
-        document.getElementById('textarea').value = localStorage.getItem('event');
-        }
-
-        //function to clear event text input and clear from local
-
-    
-
-    });
+//function to get stored events from local storage and put them on screen in their correct time blocks
