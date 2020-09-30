c8b89cd72d3b6f7a7cd15ffce59021d15a0c47f8
honeybee <63674362+mandylloyd@users.noreply.github.com>
2020-09-29 22:50:53 -0400

    made time function shorter

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,11 @@
 //updates date and time on page load
 var hoursArr = ["9am", "10am", "11am", "12pm", "1pm", "2pm,", "3pm","4pm", "5pm"] ;
 
-var datetime = null,
-        date = null;
-        time = null;
-
-var update = function () {
-    date = moment(new Date())
-    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
-};
-
-$(document).ready(function(){
-    datetime = $('#currentDay')
-    update();
-    setInterval(update, 1000);
-});
+var update = function() {
+    document.getElementById("currentDay")
+    .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+}
+setInterval(update, 1000);
     
 //TODO
 
